d8f74f02bd42e5a131a9b2e6d70107ce3d7b0825
Alex K <alexeykofficial@gmail.com>
2013-12-27 12:51:15 +0000

    Side pane now collapses when clicking on free space.

--- src/rflect/cal/ui/sidepane.js
+++ src/rflect/cal/ui/sidepane.js
@@ -10,6 +10,8 @@
 goog.provide('rflect.cal.ui.SidePane');
 
 goog.require('goog.array');
+goog.require('goog.dom');
+goog.require('goog.dom.classes');
 goog.require('goog.events.EventType');
 goog.require('goog.i18n.DateTimeFormat');
 goog.require('goog.i18n.DateTimePatterns');
@@ -23,6 +25,7 @@ goog.require('rflect.cal.EventType');
 goog.require('rflect.cal.i18n.Symbols');
 goog.require('rflect.cal.predefined');
 goog.require('rflect.cal.ui.PaneShowBehavior');
+goog.require('rflect.cal.ui.PaneShowBehavior.EventTypes');
 
 
 
@@ -75,8 +78,6 @@ rflect.cal.ui.SidePane = function(aViewManager, aTimeManager, aEventManager,
   this.showBehavior = new rflect.cal.ui.PaneShowBehavior(this, 
       this.getDomHelper().getElement('main-container'));
 
-  this.addChild(this.buttonToCalendar_ = new goog.ui.Button(null,
-      goog.ui.FlatButtonRenderer.getInstance()));
   this.addChild(this.calSelector_ = new rflect.cal.ui.CalSelector(
       this.viewManager_, this.containerSizeMonitor_, this.eventManager_));
 
@@ -90,6 +91,14 @@ goog.inherits(rflect.cal.ui.SidePane, rflect.ui.Component);
 rflect.cal.ui.SidePane.EventTypes = {
   CANCEL: 'settingsCancel'
 };
+
+
+/**
+ * Glass pane that blocks ui when side pane is shown.
+ * @type {Element}
+ * @private
+ */
+rflect.cal.ui.SidePane.prototype.glassElement_;
 
 
 /**
@@ -126,20 +135,9 @@ rflect.cal.ui.SidePane.prototype.buildInternal = function(aSb) {
 
   var parts = [
     '<nav id="side-pane" class="side-pane slide-pane-left" style="display:none">',
-    '<div class="control-pane">',
-    '<div class="pane-left">',
-    '<div class="cal-menu-button goog-flat-button goog-inline-block cal-menu-button-simple"' +
-        'id="' + rflect.cal.predefined.BUTTON_TO_CALENDAR_ID + '">',
-    '<i class="icon icon-chevron-left"></i>',
-    rflect.cal.i18n.Symbols.TO_CALENDAR,
-    '</div>',
-    '</div>',
-    '</div>',
-    '<div class="side-pane-body">',
     '<ul class="pane-menu">',
     '</ul>',
     '<div id="calendars-selector" class="list-selector">',
-    '</div>',
     '</div>',
     '</nav>'
   ];
@@ -151,10 +149,10 @@ rflect.cal.ui.SidePane.prototype.buildInternal = function(aSb) {
     aSb.append(parts[counter]);
     switch (counter) {
       // Include top pane in common buffer.
-      case 11: {
+      case 1: {
         this.buildMenu_(aSb);
       };break;
-      case 13: {
+      case 3: {
         this.calSelector_.build(aSb);
       };break;
       default: break;
@@ -167,15 +165,15 @@ rflect.cal.ui.SidePane.prototype.buildInternal = function(aSb) {
  * Decorates buttons, attaches event handlers for them.
  */
 rflect.cal.ui.SidePane.prototype.enterDocument = function() {
-
-  this.buttonToCalendar_.decorate(this.dom_.getElement(
-        rflect.cal.predefined.BUTTON_TO_CALENDAR_ID));
   
   rflect.cal.ui.SidePane.superClass_.enterDocument.call(this);
 
   // Attaching event handlers.
-  this.getHandler().listen(this.buttonToCalendar_,
-      goog.ui.Component.EventType.ACTION, this.onCancel_, false, this);
+  this.getHandler().listen(this.getGlassElement_(),
+      goog.events.EventType.CLICK, this.onCancel_, false, this)
+      .listen(this.showBehavior,
+      rflect.cal.ui.PaneShowBehavior.EventTypes.SLIDE_BREAK_POINT,
+      this.onSlideBreakPoint_, false, this);
 };
 
 
@@ -191,6 +189,38 @@ rflect.cal.ui.SidePane.prototype.onCancel_ = function() {
 
 
 /**
+ * @return {Element} Glass pane that blocks ui when side pane is shown.
+ * @private
+ */
+rflect.cal.ui.SidePane.prototype.getGlassElement_ = function() {
+  return this.glassElement_ || (this.glassElement_ = goog.dom.createDom('div',
+      'glass-pane'));
+}
+
+
+/**
+ * Slide start/stop listener.
+ * @param {rflect.cal.ui.PaneShowBehavior.SlideBreakPointEvent} aEvent Event
+ * object.
+ */
+rflect.cal.ui.SidePane.prototype.onSlideBreakPoint_ = function(aEvent) {
+  if (aEvent.showing && aEvent.start){
+    document.body.appendChild(this.getGlassElement_());
+    setTimeout(goog.bind(function(){
+      goog.dom.classes.add(this.getGlassElement_(), 'glass-pane-opaque');
+    }, this), 0);
+  }
+  if (!aEvent.showing && aEvent.start){
+    goog.dom.classes.remove(this.getGlassElement_(), 'glass-pane-opaque');
+  }
+  if (!aEvent.showing && !aEvent.start){
+    goog.dom.removeNode(this.getGlassElement_());
+  }
+
+}
+
+
+/**
  * Disposes of the component.  Calls {@code exitDocument}, which is expected to
  * remove event handlers and clean up the component.  Propagates the call to
  * the component's children, if any. Removes the component's DOM from the
@@ -200,7 +230,7 @@ rflect.cal.ui.SidePane.prototype.onCancel_ = function() {
  */
 rflect.cal.ui.SidePane.prototype.disposeInternal = function() {
   this.showBehavior.dispose();
-  this.buttonToCalendar_ = null;
+  this.glassElement_ = null;
 
   rflect.cal.ui.SidePane.superClass_.disposeInternal.call(this);
 };
