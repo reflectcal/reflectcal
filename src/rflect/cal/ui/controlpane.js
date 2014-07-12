b3c254cfc1e206ada86281a402344c4bcb5b4fe5
Alexey Komov <alexeykofficial@gmail.com>
2014-07-12 15:08:31 +0300

    Edit - flag for movable side pane is now more predictable.

--- src/rflect/cal/ui/controlpane.js
+++ src/rflect/cal/ui/controlpane.js
@@ -74,7 +74,7 @@ rflect.cal.ui.ControlPane = function(aViewManager, aTimeManager, opt_configTop) 
 
   // Add buttons. No need for captions or content here, because we've decorated
   // them.
-  if (this.configTop_ || this.configCombined_)
+  if (this.configTop_ || (this.configCombined_ && rflect.SIDE_PANE_MOVABLE))
     this.addChild(this.buttonMenu_ = new goog.ui.Button(null,
         goog.ui.FlatButtonRenderer.getInstance()));
   if (this.configBottom_ || this.configCombined_)
@@ -202,13 +202,19 @@ rflect.cal.ui.ControlPane.prototype.buildInternal = function(aSb) {
 
     parts = [
       '<div id="top-pane" class="control-pane">',
-      '<div id="sidebar-controls">',
-      // First button container.
-      // 'Menu' button.
-      '<div class="cal-menu-button goog-flat-button goog-inline-block"' +
-          'id="' + rflect.cal.predefined.BUTTON_MENU_ID + '">' +
-          '<div class="icon icon-in-button icon-bars goog-inline-block"></div>',
-      '</div>',
+      '<div id="sidebar-controls">'
+    ];
+    if (rflect.SIDE_PANE_MOVABLE) {
+      parts = parts.concat([
+        // First button container.
+        // 'Menu' button.
+        '<div class="cal-menu-button goog-flat-button goog-inline-block"' +
+            'id="' + rflect.cal.predefined.BUTTON_MENU_ID + '">' +
+            '<div class="icon icon-in-button icon-bars goog-inline-block"></div>',
+        '</div>'
+      ]);
+    }
+    parts = parts.concat([
       // 'Now' button.
       '<div id="' + rflect.cal.predefined.BUTTON_NOW_ID +
           '" class="' + goog.getCssName('goog-flat-button-collapse-right') + ' ',
@@ -265,8 +271,7 @@ rflect.cal.ui.ControlPane.prototype.buildInternal = function(aSb) {
       '</div></div>',
       '</div></div>',
       '</div>'
-    ];
-
+    ]);
   }
 
   // Form html. From index 1, because 0 is the html of outer container, which
