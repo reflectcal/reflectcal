29356522883f3a218839ee68c35623f28392d709
Alexey Komov <alexeykcontact@gmail.com>
2024-10-30 12:23:12 +0300

    Revert "fix: add server logs"
    This reverts commit 9478c7b74759e025cbbd55d526945b28bdec33f3.

--- app/db/entity.js
+++ app/db/entity.js
@@ -7,12 +7,14 @@
  * @author alexeykofficial@gmail.com (Alex K.)
  */
 
+
 var db = require('./connection').db;
 var dbUtil = require('./util');
 var deepClone = require('clone');
 var appConfig = require('../config/appconfig');
 var log = appConfig.log;
 var Q = require('q');
+
 
 /**
  * Loads entities.
@@ -27,35 +29,30 @@ var Q = require('q');
  * no entities are present.
  */
 exports.getEntitiesAsync = function(aCollectionName, aLookupObject,
-                                    aOnEntitiesLoad, aEntityToTransportJSON, opt_defaultEntity){
-  log.info(`Fetching entities from collection: ${aCollectionName}`, { lookup: aLookupObject });
-
+    aOnEntitiesLoad, aEntityToTransportJSON, opt_defaultEntity){
+  log.info('getEntitiesAsync');
   var entities = [];
   var collection = db.get(aCollectionName);
 
   var onEnsureEntityExists = function() {
     collection.find(aLookupObject, {}, function(aError, aEntities){
-      if (aError) {
-        log.error(`Error fetching entities from ${aCollectionName}`, aError);
-        return;
-      }
-
       aEntities && aEntities.forEach(function(aEntity) {
         entities.push(aEntityToTransportJSON(aEntity));
       });
 
-      log.info(`Fetched ${entities.length} entities from ${aCollectionName}`);
+      // Executing callback for view.
       aOnEntitiesLoad(entities);
     });
   }
 
-  if (opt_defaultEntity) {
-    log.info(`Ensuring at least one entity exists in ${aCollectionName}`);
-    ensureEntityExists(collection, aLookupObject, onEnsureEntityExists, opt_defaultEntity);
-  } else {
+  // If we need to be sure that at least one entity exits, check that.
+  if (opt_defaultEntity)
+    ensureEntityExists(collection, aLookupObject, onEnsureEntityExists,
+        opt_defaultEntity);
+  else
     onEnsureEntityExists();
-  }
 };
+
 
 /**
  * Ensures that entity exists.
@@ -64,95 +61,93 @@ exports.getEntitiesAsync = function(aCollectionName, aLookupObject,
  * @param {function()} aOnEnsureEntityExist Callback that will be executed
  * when entity is truly present.
  */
-function ensureEntityExists(aCollection, aLookupObject, aOnEnsureEntityExist, aDefaultEntity){
-  log.info(`Checking if entity exists in collection with lookup:`, { lookup: aLookupObject });
-
+function ensureEntityExists(aCollection, aLookupObject,
+    aOnEnsureEntityExist, aDefaultEntity){
+  log.info('ensureEntityExists');
   aCollection.count(aLookupObject, function(aError, aCount){
-    if (aError) {
-      log.error(`Error counting entities in collection`, aError);
-      return;
-    }
+    log.info('aCount', aCount );
 
-    log.info(`Found ${aCount} entities matching lookup in collection`);
+    if (aCount == 0) dbUtil.getUniqueIdAsync(aCollection, function(aId) {
+      var defaultEntity = deepClone(aDefaultEntity);
+      defaultEntity._id = aId;
 
-    if (aCount === 0) {
-      log.info(`No entities found, inserting default entity`, aDefaultEntity);
-      dbUtil.getUniqueIdAsync(aCollection, function(aId) {
-        var defaultEntity = deepClone(aDefaultEntity);
-        defaultEntity._id = aId;
-
-        aCollection.insert(defaultEntity, {}, function(aError, aResult){
-          if (aError) {
-            log.error(`Error inserting default entity`, aError);
-            return;
-          }
-          log.info(`Inserted default entity with ID ${aId}`);
-          aOnEnsureEntityExist();
-        });
+      aCollection.insert(defaultEntity, {}, function(aError, aResult){
+        aOnEnsureEntityExist();
       });
-    } else {
+    })
+    else if (aCount >= 1)
       aOnEnsureEntityExist();
-    }
   });
 }
 
+
 /**
- * Loads entities with a Promise.
+ * Loads entities.
+ * @param {string} aCollectionName Collection to which entity belongs.
+ * @param {Object} aLookupObject Object with entity lookup params.
+ * @param {function(Array)} aOnEntitiesLoad Callback that will be executed
+ * when db request is ready.
+ * @param {function(Object):Object} aEntityToTransportJSON Function that
+ * transforms db representation of entity to its transportable form.
+ * @param {Object=} opt_defaultEntity Optional - default entity that should be
+ * inserted when none exists. If omitted, no check is performed, so it's ok when
+ * no entities are present.
  */
 exports.getEntitiesWithPromise = function(aCollectionName, aLookupObject,
-                                          aEntityToTransportJSON, opt_defaultEntity){
-  log.info(`Fetching entities with promise from collection: ${aCollectionName}`, { lookup: aLookupObject });
-
+    aEntityToTransportJSON, opt_defaultEntity){
   return new Promise(function(resolve, reject) {
     var collection = db.get(aCollectionName);
-
-    ensureEntityExistsWithPromise(collection, aLookupObject, opt_defaultEntity)
+    ensureEntityExistsWithPromise(collection, aLookupObject,
+        opt_defaultEntity)
         .then(function() {
           var findWithPromise = Q.denodeify(collection.find.bind(collection));
           return findWithPromise(aLookupObject, {});
         })
         .then(function(aEntities) {
-          var entities = aEntities.map(aEntityToTransportJSON);
-          log.info(`Fetched ${entities.length} entities from ${aCollectionName}`);
+          var entities = [];
+          aEntities.forEach(function(aEntity) {
+            entities.push(aEntityToTransportJSON(aEntity));
+          });
           resolve(entities);
-        }, function(error) {
-          log.error(`Error fetching entities with promise from ${aCollectionName}`, error);
-          reject(error);
-        });
+        }, reject);
   });
 }
 
+
 /**
- * Ensures that entity exists with a Promise.
+ * Ensures that entity exists.
+ * @param {Object} aCollection Collection to which entity belongs.
+ * @param {Object} aLookupObject Object with entity lookup params.
+ * @param {function()} aOnEnsureEntityExist Callback that will be executed
+ * when entity is truly present.
  */
-function ensureEntityExistsWithPromise(aCollection, aLookupObject, aDefaultEntity){
+function ensureEntityExistsWithPromise(aCollection, aLookupObject,
+    aDefaultEntity){
   return new Promise(function(resolve, reject) {
     if (!aDefaultEntity) {
       resolve();
     } else {
       var countWithPromise = Q.denodeify(aCollection.count.bind(aCollection));
       countWithPromise(aLookupObject).then(function(aCount) {
-        if (aCount === 0) {
-          log.info(`No entities found, inserting default entity`, aDefaultEntity);
-          return dbUtil.getUniqueIdAsyncWithPromise(aCollection)
-              .then(function(aId) {
-                return insertDefaultEntityWithPromise(aCollection, aDefaultEntity, aId);
-              })
-              .then(resolve, reject);
-        } else {
-          log.info(`Entity already exists, count: ${aCount}`);
+        if (aCount == 0) {
+          dbUtil.getUniqueIdAsyncWithPromise(aCollection).
+              then(function(aId) {
+                return insertDefaultEntityWithPromise(aCollection,
+                    aDefaultEntity, aId);
+              }).
+              then(resolve, reject);
+        } else if (aCount >= 1) {
           resolve();
+        } else {
+          reject(new Error('Meaningless count.'));
         }
-      }).catch(function(error) {
-        log.error(`Error ensuring entity exists in collection`, error);
-        reject(error);
-      });
-    }
+      }, reject);
+    } 
   });
 }
 
+
 function insertDefaultEntityWithPromise(aCollection, aDefaultEntity, aId) {
-  log.info(`Inserting default entity with generated ID ${aId}`);
   var defaultEntity = deepClone(aDefaultEntity);
   defaultEntity._id = aId;
 
@@ -160,62 +155,53 @@ function insertDefaultEntityWithPromise(aCollection, aDefaultEntity, aId) {
   return insertWithPromise(defaultEntity, {});
 }
 
-/**
- * Saves an entity.
- */
-exports.saveEntityAsync = function(aCollectionName, aEntityJSON, aOnEntitySave, aEntityFromTransportJSON){
-  log.info(`Saving entity to collection: ${aCollectionName}`, aEntityJSON);
 
+/**
+ * Saves entity.
+ * @param {string} aCollectionName name of collection to which entity belongs.
+ * @param {Object} aEntityJSON JSON representing entity.
+ * @param {function(string|number)} aOnEntitySave Callback that will be
+ * called when db request is ready.
+ * @param {function(Object):Object} aEntityFromTransportJSON Function that
+ * transforms transportable form of entity to its db representation.
+ */
+exports.saveEntityAsync = function(aCollectionName, aEntityJSON, aOnEntitySave,
+    aEntityFromTransportJSON){
   var collection = db.get(aCollectionName);
   var entity = aEntityFromTransportJSON(aEntityJSON);
 
   collection.count({ _id: entity._id }, function(aError, aCount){
-    if (aError) {
-      log.error(`Error counting entities in ${aCollectionName}`, aError);
-      return;
-    }
-
-    if (aCount === 0) {
-      dbUtil.getUniqueIdAsync(collection, function(aUniqueId){
-        entity._id = aUniqueId;
-        collection.insert(entity, {}, function(aError, aResult){
-          if (aError) {
-            log.error(`Error inserting new entity`, aError);
-            return;
-          }
-          log.info(`Inserted new entity with ID ${aUniqueId}`);
-          aOnEntitySave(aUniqueId);
-        });
+    if (aCount == 0) dbUtil.getUniqueIdAsync(collection,
+        function(aUniqueId){
+      entity._id = aUniqueId;
+      collection.insert(entity, {}, function(aError, aResult){
+        // Passing new id to callback.
+        aOnEntitySave(aUniqueId);
       });
-    } else {
-      collection.update({ _id: entity._id }, entity, {}, function(aError, aResult){
-        if (aError) {
-          log.error(`Error updating entity`, aError);
-          return;
-        }
-        log.info(`Updated existing entity with ID ${entity._id}`);
+    })
+    else if (aCount > 0)
+      collection.update({ _id: entity._id }, entity, {},
+          function(aError, aResult){
+        // Signalizing that update was ok.
         aOnEntitySave(0);
       });
-    }
   });
 };
 
-/**
- * Deletes an entity.
- */
-exports.deleteEntityAsync = function(aCollectionName, aEntityId, aOnEntityDelete){
-  log.info(`Deleting entity with ID ${aEntityId} from collection: ${aCollectionName}`);
 
+/**
+ * Deletes entity.
+ * @param {string} aCollectionName Name of collection to which entity belongs.
+ * @param {string} aEntityId Entity id.
+ * @param {function(number)} aOnEntityDelete Callback that will be called
+ * when db request is ready.
+ */
+exports.deleteEntityAsync = function(aCollectionName, aEntityId, 
+    aOnEntityDelete){
   var collection = db.get(aCollectionName);
   collection.remove({ _id: aEntityId }, {}, function(aError, aNumberOfDeleted){
-    if (aError) {
-      log.error(`Error deleting entity`, aError);
-      aOnEntityDelete(-1);
-      return;
-    }
-
+    // Passing result to callback.
     var result = aNumberOfDeleted > 0 ? 0 : -1;
-    log.info(`Entity with ID ${aEntityId} ${result === 0 ? 'deleted' : 'not found'}`);
     aOnEntityDelete(result);
   });
 };
