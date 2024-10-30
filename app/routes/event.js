4715c48bb17079db9d4bc0de1542491d7f144212
Alexey Komov <alexeykcontact@gmail.com>
2024-10-30 11:03:34 +0300

    feat: add logs for /events routes

--- app/routes/event.js
+++ app/routes/event.js
@@ -7,41 +7,73 @@
  * @author alexeykofficial@gmail.com (Alex K.)
  */
 
-
 var eventDAO = require('../db/event');
-
+var appConfig = require('../config/appconfig');
+var log = appConfig.log;
 
 /**
  * Loads events.
  */
-exports.eventsLoad = function(req, res){
+exports.eventsLoad = function(req, res) {
+  log.info('Received request to load events', { requestBody: req.body });
+
   var onEventsLoad = function(aEvents) {
+    log.info(`Loaded ${aEvents.length} events`);
     res.send(JSON.stringify(aEvents));
-  }
+  };
 
-  eventDAO.getEventsAsync(req.body, onEventsLoad);
-}
-
+  eventDAO.getEventsAsync(req.body, function(aError, aEvents) {
+    if (aError) {
+      log.error('Error loading events', aError);
+      res.status(500).send({ error: 'Failed to load events' });
+    } else {
+      onEventsLoad(aEvents);
+    }
+  });
+};
 
 /**
  * Saves event.
  */
-exports.eventSave = function(req, res){
+exports.eventSave = function(req, res) {
+  log.info('Received request to save event', { eventData: req.body });
+
   var onEventSave = function(aEventId) {
+    log.info(`Event saved successfully with ID ${aEventId}`);
     res.send(JSON.stringify(aEventId));
-  }
+  };
 
-  eventDAO.saveEventAsync(req.body, onEventSave);
+  eventDAO.saveEventAsync(req.body, function(aError, aEventId) {
+    if (aError) {
+      log.error('Error saving event', aError);
+      res.status(500).send({ error: 'Failed to save event' });
+    } else {
+      onEventSave(aEventId);
+    }
+  });
 };
-
 
 /**
  * Deletes event.
  */
-exports.eventDelete = function(req, res){
-  var onEventDelete = function(aResult) {
-    res.send(JSON.stringify(aResult));
-  }
+exports.eventDelete = function(req, res) {
+  log.info('Received request to delete event', { eventId: req.params.id });
 
-  eventDAO.deleteEventAsync(req.params.id, onEventDelete);
+  var onEventDelete = function(aResult) {
+    if (aResult === 0) {
+      log.info(`Event with ID ${req.params.id} deleted successfully`);
+    } else {
+      log.warn(`Event with ID ${req.params.id} not found`);
+    }
+    res.send(JSON.stringify(aResult));
+  };
+
+  eventDAO.deleteEventAsync(req.params.id, function(aError, aResult) {
+    if (aError) {
+      log.error('Error deleting event', aError);
+      res.status(500).send({ error: 'Failed to delete event' });
+    } else {
+      onEventDelete(aResult);
+    }
+  });
 };
